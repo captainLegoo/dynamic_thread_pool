b157225610109950a24a4db04ebda79ee542bfa2
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-17 22:43:42 +0100

    init repo

---
+++ dynamic-thread-pool-test/src/main/java/cn/dcy/threadpool/DynamicThreadPoolTest.java
@@ -0,0 +1,15 @@
+package cn.dcy.threadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kyle
+ * @date 2024/06/17
+ */
+@SpringBootApplication
+public class DynamicThreadPoolTest {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicThreadPoolTest.class, args);
+    }
+}
